067fd1ae87eca259e5ed6f527e8c5bfa7d1f96bf
Anushka404 <anushkatewari2004@gmail.com>
2025-08-05 22:48:37 +0530

    analytics to deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const montserrat = Montserrat({
     subsets: ['latin'],
@@ -34,6 +35,7 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased ${montserrat.variable} font-sans`}>
                 {children}
+                <Analytics />
             </body>
         </html>
     );
